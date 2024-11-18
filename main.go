9c5de27fea464448569104d4673b9827314f20b2
Raiwin Inthasit <andaraiwin@gmail.com>
2024-11-18 17:14:17 +0700

    install dependencies for swaggo

--- main.go
+++ main.go
@@ -1,6 +1,20 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	_ "DAF-Bridge/Talent-Atmos-Backend/docs"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// @title Book API
+// @description This is a sample server for a book API.
+// @version 1.0
+// @host localhost:8080
+// @BasePath /
+// @schemes http
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 
 func main() {
 	app := fiber.New()
@@ -9,5 +23,22 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(":8080")
+	// app.Get("/swagger/*", swagger.HandlerDefault) // default
+
+	// app.Get("/swagger/*", swagger.New(swagger.Config{ // custom
+	// 	URL:         "http://example.com/doc.json",
+	// 	DeepLinking: false,
+	// 	// Expand ("list") or Collapse ("none") tag groups by default
+	// 	DocExpansion: "none",
+	// 	// Prefill OAuth ClientId on Authorize popup
+	// 	OAuth: &swagger.OAuthConfig{
+	// 		AppName:  "OAuth Provider",
+	// 		ClientId: "21bb4edc-05a7-4afc-86f1-2e151e4ba6e2",
+	// 	},
+	// 	// Ability to change OAuth2 redirect uri location
+	// 	OAuth2RedirectUrl: "http://localhost:8080/swagger/oauth2-redirect.html",
+	// }))
+
+	// app.Listen(":8080")
 }
