0bfa12d9947e5bbc0026c761802ae1ac0bcee0c6
Wuttipat Sanchai <wuttipat.gamxu@gmail.com>
2024-11-20 16:21:13 +0700

    improve migrate directory

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/DAF-Bridge/Talent-Atmos-Backend/initializers"
 	"github.com/DAF-Bridge/Talent-Atmos-Backend/internal/domain"
 )
 
 func init() {
+	//  Load environment variables
 	initializers.LoadEnvVar()
+	// Connect to database
+	initializers.ConnectToDB()
 }
 
 func main() {
-	db := initializers.ConnectToDB()
-	db.AutoMigrate(&domain.User{})
+	if initializers.DB == nil {
+        log.Fatal("Database connection is not established.")
+    }
+	initializers.DB.AutoMigrate(&domain.User{})
 }
