b464a76a644253b7b66c46efee32f8e779a0f994
Wuttipat Sanchai <wuttipat.gamxu@gmail.com>
2024-12-02 15:00:32 +0700

    migrated org model and it related

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -13,7 +13,7 @@ func init() {
 	// Connect to database
 	initializers.ConnectToDB()
 	// Create enum types
-	// initializers.InitEnums(initializers.DB)
+	initializers.InitEnums(initializers.DB)
 }
 
 func main() {
@@ -21,19 +21,10 @@ func main() {
 		log.Fatal("Database connection is not established.")
 	}
 
-	initializers.DB.AutoMigrate(&domain.User{})
-	initializers.DB.AutoMigrate(&domain.Profile{})
-	// initializers.DB.Migrator().DropColumn(&domain.Profile{}, "pic_url")
 
-	// if err := initializers.DB.AutoMigrate(&domain.User{}); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// initializers.DB.Create(&domain.Profile{
-	// 	FirstName: "John",
-	// 	LastName:  "Doe",
-	// 	Email:     "test@gmail.com",
-	// 	Phone:     "1234567890",
-	// 	PicUrl:    "https://drive.google.com/uc?export=view&id=1-wqxOT_uo1pE_mEPHbJVoirMMH2Be3Ks",
-	// 	UserID:    1,
-	// })
+	initializers.DB.AutoMigrate(&domain.Organization{})
+	initializers.DB.AutoMigrate(&domain.OrganizationContact{})
+	initializers.DB.AutoMigrate(&domain.OrgOpenJob{})
+
+
 }
