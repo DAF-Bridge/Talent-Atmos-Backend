965671c086666e8476af5167332a16f41f6d7cf0
Raiwin Inthasit <andaraiwin@gmail.com>
2025-01-18 22:42:05 +0700

    Add S3 infrastructure

---
+++ internal/infrastructure/s3.go
@@ -0,0 +1,58 @@
+package infrastructure
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"mime/multipart"
+	"path/filepath"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type S3Uploader struct {
+	client     *s3.Client
+	bucketName string
+}
+
+func NewS3Uploader(bucketName string) *S3Uploader {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	return &S3Uploader{
+		client:     client,
+		bucketName: bucketName,
+	}
+}
+
+// Upload file to S3
+func (s *S3Uploader) UploadFile(ctx context.Context, file multipart.File, fileHeader *multipart.FileHeader, userID string) (string, error) {
+	fileExt := filepath.Ext(fileHeader.Filename)
+	objectKey := fmt.Sprintf("users/%s_%d%s", userID, time.Now().Unix(), fileExt)
+
+	buffer := bytes.NewBuffer(nil)
+	if _, err := buffer.ReadFrom(file); err != nil {
+		return "", fmt.Errorf("failed to read file: %w", err)
+	}
+
+	_, err := s.client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(s.bucketName),
+		Key:    aws.String(objectKey),
+		Body:   buffer,
+		ACL:    "public-read",
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to upload file: %w", err)
+	}
+
+	fileURL := fmt.Sprintf("https://%s.s3.amazonaws.com/%s", s.bucketName, objectKey)
+	return fileURL, nil
+}
