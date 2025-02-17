7d93835fa85803942f21b562214a8d06a0d03e5c
Raiwin Inthasit <andaraiwin@gmail.com>
2025-02-17 17:38:44 +0700

    test(event): fix integration test

--- internal/test/integration/event_integration_test.go
+++ internal/test/integration/event_integration_test.go
@@ -39,6 +39,7 @@ func TestEventHandlerIntegrationService(t *testing.T) {
 			EndDate:        utils.DateOnly{Time: utils.DateParser("2025-01-16")},
 			StartTime:      utils.TimeOnly{Time: utils.TimeParser("09:00:00")},
 			EndTime:        utils.TimeOnly{Time: utils.TimeParser("17:00:00")},
+			Content:        "Explore advancements in renewable energy technologies.",
 			LocationName:   "Conference Hall A",
 			Latitude:       13.7563,
 			Longitude:      100.5018,
