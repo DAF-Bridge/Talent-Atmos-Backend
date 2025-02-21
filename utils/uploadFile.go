995f4a88f2f2e08d1933ddfb303bafd6dfccca40
Raiwin Inthasit <andaraiwin@gmail.com>
2025-02-21 22:11:49 +0700

    feat(upload bg image): add utils function handle context upload background image from client

--- utils/uploadFile.go
+++ utils/uploadFile.go
@@ -11,10 +11,30 @@ import (
 func UploadImage(c *fiber.Ctx) (multipart.File, *multipart.FileHeader, error) {
 	var file multipart.File
 	var fileHeader *multipart.FileHeader
+
 	fileHeader, err := c.FormFile("image")
 	if err != nil {
 		logs.Error(err)
 		return nil, nil, errs.NewBadRequestError("Failed to get image from form")
+	}
+
+	file, err = fileHeader.Open()
+	if err != nil {
+		logs.Error(err)
+		return nil, nil, errs.NewUnexpectedError()
+	}
+
+	return file, fileHeader, nil
+}
+
+func UploadBackgroundImage(c *fiber.Ctx) (multipart.File, *multipart.FileHeader, error) {
+	var file multipart.File
+	var fileHeader *multipart.FileHeader
+
+	fileHeader, err := c.FormFile("background_image")
+	if err != nil {
+		logs.Error(err)
+		return nil, nil, errs.NewBadRequestError("Failed to get background image from form")
 	}
 
 	file, err = fileHeader.Open()
